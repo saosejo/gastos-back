7e66bd121adcb0c8539e26081048c07371d09138
Santiago Osejo <saosejo@hotmail.com>
2024-11-14 22:39:42 -0500

    initial

---
+++ routes/userRoutes.js
@@ -0,0 +1,53 @@
+// routes/userRoutes.js
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const { authMiddleware, signToken }  = require('../service/authMiddleware');
+
+const router = express.Router();
+
+// Login API
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: 'Invalid credentials' });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: 'Invalid credentials' });
+  }
+
+  // Create JWT Token
+  const token = signToken(user); 
+  res.json({ token, message: 'Login successful', user: user });
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+      
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ email, password: hashedPassword });
+    // Create new user
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully', user: newUser });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors'); // Import the cors library
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000', // Replace with your frontend's origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Specify allowed headers
+}));
+// Connect to MongoDB (using cloud connection string)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB Cloud'))
+.catch((error) => console.error('MongoDB connection error:', error));
+
+const userRoutes = require('./routes/userRoutes');
+const listRoutes = require('./routes/listRoutes');
+app.use('/api/users', userRoutes);
+app.use('/api/list', listRoutes);
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
